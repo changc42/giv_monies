950807b2c1c2d46983715b54ba8369e242c661f0
Caleb Chang <changc42@gmail.com>
2021-02-23 00:27:43 -0500

    added env var in frontend directory

--- frontend/src/config/prod.js
+++ frontend/src/config/prod.js
@@ -1,4 +1,6 @@
 export default {
-  BACKEND_URL: "https://peaceful-dawn-30217.herokuapp.com",
   FRONTEND_URL: "https://peaceful-dawn-30217.herokuapp.com",
+  BACKEND_URL: "https://peaceful-dawn-30217.herokuapp.com",
+  STRIPE_PK: process.env.STRIPE_PK,
+  STRIPE_SK: process.env.STRIPE_SK,
 };
